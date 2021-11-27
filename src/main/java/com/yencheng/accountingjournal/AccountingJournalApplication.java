968d041a83c9393ecbe9b5f88a486ed2e73e5f83
Yencheng Liu <c794613852852@gmail.com>
2021-11-27 15:52:27 +0900

    initial project

---
+++ src/main/java/com/yencheng/accountingjournal/AccountingJournalApplication.java
@@ -0,0 +1,13 @@
+package com.yencheng.accountingjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yencheng/accountingjournal/AccountingJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yencheng.accountingjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
